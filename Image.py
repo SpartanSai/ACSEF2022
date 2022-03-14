e862bb139c3331f0cb5ca7aaddf49da74c7b6e48
Varun <varrao23@gmail.com>
2022-03-13 23:17:36 -0700

    Modified code

--- Image.py
+++ Image.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-# Load Yolo
+def distancing(box = []):
+    cx, cy, x, y, w, h = box
+    return True
+
 net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
 with open("coco.names", "r") as f:
@@ -10,18 +13,15 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-# Loading image
-img = cv2.imread("room_ser.jpg")
+img = cv2.imread("people1.png")
 img = cv2.resize(img, None, fx=0.4, fy=0.4)
 height, width, channels = img.shape
 
-# Detecting objects
 blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
 
 net.setInput(blob)
 outs = net.forward(output_layers)
 
-# Showing informations on the screen
 class_ids = []
 confidences = []
 boxes = []
@@ -30,31 +30,29 @@ for out in outs:
         scores = detection[5:]
         class_id = np.argmax(scores)
         confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
+        if confidence > 0.5 and str(classes[class_id]) == 'person':
             center_x = int(detection[0] * width)
             center_y = int(detection[1] * height)
             w = int(detection[2] * width)
             h = int(detection[3] * height)
-
-            # Rectangle coordinates
             x = int(center_x - w / 2)
             y = int(center_y - h / 2)
 
-            boxes.append([x, y, w, h])
+            boxes.append([center_x, center_y, x, y, w, h])
             confidences.append(float(confidence))
             class_ids.append(class_id)
 
 indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 print(indexes)
-font = cv2.FONT_HERSHEY_PLAIN
 for i in range(len(boxes)):
     if i in indexes:
-        x, y, w, h = boxes[i]
+        cx, cy, x, y, w, h = boxes[i]
         label = str(classes[class_ids[i]])
-        color = colors[class_ids[i]]
+        if distancing(boxes[i]):
+            color = (0, 255, 0)
+        else:
+            color = (0, 0, 255)
         cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
 
 
 cv2.imshow("Image", img)
